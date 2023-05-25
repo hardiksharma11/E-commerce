aa6f07e5bb8019f93e62a8602a1bc496c5f3057a
Hardik <hardik182002@gmail.com>
2023-05-25 23:30:51 +0530

    fixed bugs

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const mysql = require('mysql2');
-const { redirect } = require("react-router-dom");
 const path = require("path")
 const cors = require("cors");
 var nodemailer = require('nodemailer');
